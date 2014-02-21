c794f0e26db29990e88f859b6b5874e460fe313d
Ian Duffy <ian@ianduffy.ie>
2014-02-21 20:39:42 +0000

    init

--- ec2stack/models/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from ec2stack.models.users.models import *
--- ec2stack/services.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from ec2stack.models.users import UsersService
-
-
-USERS = UsersService()
